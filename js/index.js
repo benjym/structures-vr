1ccb14d3b19893501c14b1b3a191b663b05a7799
benjym <benjy.marks@sydney.edu.au>
2022-03-17 02:33:57 +0000

    Deploying to gh-pages from @ benjym/structures-vr@6effc5bef262188a8befd40deb8ff2f6976b5164 🚀

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
+import css from "../css/main.css";
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import { Lut } from 'three/examples/jsm/math/Lut.js';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
 
 let beam, initial_positions;
 let left_support, right_support;
 let load_position_gui;
 let EI;
+let controller1, controller2;
+
+let urlParams = new URLSearchParams(window.location.search);
 
 let params = {
     length : 10, // beam length (m)
@@ -23,12 +30,24 @@ let params = {
 
 let lut;
 let rainbow = new Lut("rainbow", 512); // options are rainbow, cooltowarm and blackbody
-let cooltowarm = new Lut("rainbow", 512); // options are rainbow, cooltowarm and blackbody
+let cooltowarm = new Lut("cooltowarm", 512); // options are rainbow, cooltowarm and blackbody
 // lut.setMin(0);
 // lut.setMax(100);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+if ( urlParams.has('VR') ) {
+    document.body.appendChild( VRButton.createButton( renderer ) );
+    renderer.xr.enabled = true;
+    add_controllers();
+}
+
+const gridHelper = new THREE.GridHelper( 100, 100 );
+scene.add( gridHelper );
 
 const light = new THREE.AmbientLight( 0xAAAAAA );
 scene.add(light);
@@ -36,11 +55,6 @@ scene.add(light);
 const light2 = new THREE.PointLight( 0xAAAAAA );
 light2.position.set( 10, 5, 10);
 scene.add(light2);
-
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
@@ -62,6 +76,7 @@ gui.add( params, 'colour_by', ['None','Shear Force','Bending Moment'] )
     .name( 'Colour by' ).onChange( redraw_beam );
 
 make_new_beam();
+
 function make_new_beam() {
     if ( beam !== undefined ) {
         scene.remove(beam)
@@ -105,7 +120,38 @@ function make_new_beam() {
         scene.add( right_support );
     }
 
+    gridHelper.position.y = -2*pin_radius-params.height/2.;
     redraw_beam();
+}
+
+function onSelectStart() {
+
+}
+
+function onSelectEnd() {
+
+}
+
+function add_controllers() {
+    controller1 = renderer.xr.getController( 0 );
+	controller1.addEventListener( 'selectstart', onSelectStart );
+	controller1.addEventListener( 'selectend', onSelectEnd );
+	scene.add( controller1 );
+
+	controller2 = renderer.xr.getController( 1 );
+	controller2.addEventListener( 'selectstart', onSelectStart );
+	controller2.addEventListener( 'selectend', onSelectEnd );
+	scene.add( controller2 );
+
+	const controllerModelFactory = new XRControllerModelFactory();
+
+	let controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+	controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+	scene.add( controllerGrip1 );
+
+	let controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+	controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+	scene.add( controllerGrip2 );
 }
 
 function redraw_beam() {
@@ -176,8 +222,15 @@ function redraw_beam() {
 }
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    if ( urlParams.has('VR') ) {
+        renderer.setAnimationLoop( function () {
+            renderer.render( scene, camera );
+        } );
+    } else {
+        requestAnimationFrame( animate );
+    	renderer.render( scene, camera );
+    }
+
 };
 
 animate();
